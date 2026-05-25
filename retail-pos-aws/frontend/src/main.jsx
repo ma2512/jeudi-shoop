a3fdefc742619716324e8e150d48409316d10dc4
ma2512 <al22020338@itsa.edu.mx>
2026-05-24 18:33:51 -0600

    Diseño actualizado JeudiShop

--- retail-pos-aws/frontend/src/main.jsx
+++ retail-pos-aws/frontend/src/main.jsx
@@ -6,8 +6,8 @@ import { Amplify } from 'aws-amplify';
 Amplify.configure({
   Auth: {
     Cognito: {
-      userPoolId: 'us-west-2_0ky8mUdpE', 
-      userPoolClientId: '687kvqb86o2mpsqqmrni8di5sv', // <--- ID Actualizado aquí
+      userPoolId: 'us-west-2_SAazwJL7u', 
+      userPoolClientId: '6d5729jibb5pnpmqvsa10a5fmk',
       loginWith: {
         email: true
       }
